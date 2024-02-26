545bdce8557c6feac75aa431bbc86882870cb8f0
DergevorkianJadeCSUN <jade.dergevorkian.125@my.csun.edu>
2024-02-25 20:25:13 -0800

    added reservation table

--- startup.py
+++ startup.py
@@ -5,6 +5,7 @@ connection = sqlite3.connect("database/reservations.db")
 
 crsr = connection.cursor()
 
+#set up the tables all nice and stuff 
 sql_command = """CREATE TABLE room(
 room_number INTEGER PRIMARY KEY,
 room_type VARCHAR(10),
@@ -15,6 +16,18 @@ reserved BOOLEAN)
 
 crsr.execute(sql_command)
 
+# set up reservation table, dont add anything though, that's handled by main software
+sql_command = """CREATE TABLE reservations(
+res_num INTEGER PRIMARY KEY,
+room_num INTEGER,
+guests INTEGER,
+check_in TEXT,
+check_out TEXT)
+"""
+
+crsr.execute(sql_command)
+
+#add the 9 rooms
 sql_command = """INSERT INTO room VALUES(101, "basic", 1, 50, FALSE)"""
 crsr.execute(sql_command)
 sql_command = """INSERT INTO room VALUES(102, "basic", 1, 50, FALSE)"""
@@ -33,6 +46,8 @@ sql_command = """INSERT INTO room VALUES(302, "suite", 2, 200, FALSE)"""
 crsr.execute(sql_command)
 sql_command = """INSERT INTO room VALUES(303, "suite", 2, 200, FALSE)"""
 crsr.execute(sql_command)
+
+
 
 connection.commit()
 
